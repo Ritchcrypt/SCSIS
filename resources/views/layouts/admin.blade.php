01d422a79bec2d33455d847eb8766211bd32c6b5
Ritche O. Deroy <ritchd2003@gmail.com>
2026-07-27 20:01:55 +0800

    Complete TabangNow authentication UI and shared favicon setup

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,6 +39,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>TabangNow</title>
+    <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/tabangnow-tab-icon-v7.png?v=11"
+>
+
+<link
+    rel="shortcut icon"
+    type="image/png"
+    href="/tabangnow-tab-icon-v7.png?v=11"
+>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,21 @@
 
 <title>{{ $title ?? config('app.name', 'TabangNow') }}</title>
 
+<link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="{{ asset('tabangnow-tab-icon-v7.png') }}?v=10"
+>
+
+<link
+    rel="shortcut icon"
+    type="image/png"
+    href="{{ asset('tabangnow-tab-icon-v7.png') }}?v=10"
+>
+
 <link rel="preconnect" href="https://fonts.bunny.net">
+
 <link
     href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,800,900"
     rel="stylesheet"
