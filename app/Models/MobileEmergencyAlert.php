e3a1f0efd4e8cfb59a367f39c021ad662f8ebab7
Ritche O. Deroy <ritchd2003@gmail.com>
2026-08-15 11:51:03 +0800

    fix: include installation id on emergency alert model

--- app/Models/MobileEmergencyAlert.php
+++ app/Models/MobileEmergencyAlert.php
@@ -14,6 +14,7 @@ class MobileEmergencyAlert extends Model
         'alert_code',
         'device_id',
         'user_id',
+        'installation_id',
         'request_id',
         'status',
         'latitude',
